a70a7d5dd1cf5c6c885954f94511423070180b4f
mydongistiny <jaysonedson@gmail.com>
2016-08-01 19:53:33 -0700

    Revert "libm: Use compiler builtins for nearbyint"
    This reverts commit 3e89b7df5504380ea8db2dd3ebb93bea2ab50c37.
    
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- libm/Android.mk
+++ libm/Android.mk
@@ -127,6 +127,7 @@ LOCAL_SRC_FILES := \
     upstream-freebsd/lib/msun/src/s_modf.c \
     upstream-freebsd/lib/msun/src/s_modff.c \
     upstream-freebsd/lib/msun/src/s_nan.c \
+    upstream-freebsd/lib/msun/src/s_nearbyint.c \
     upstream-freebsd/lib/msun/src/s_nextafter.c \
     upstream-freebsd/lib/msun/src/s_nextafterf.c \
     upstream-freebsd/lib/msun/src/s_remquo.c \
@@ -250,7 +251,6 @@ LOCAL_SRC_FILES += \
 # Home-grown stuff.
 LOCAL_SRC_FILES += \
     fabs.cpp \
-    nearbyint.cpp \
 
 # Arch specific optimizations.
 
